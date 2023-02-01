026a7a3bc041e76a28cddf2c6a6d9e7f21d0366a
FlyRexy <eferovnik@mail.ru>
2023-02-01 19:48:38 +0400

    end of initial tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,2 @@
 class ApplicationController < ActionController::Base
-  def lol
-
-  end
-
-  def test2
-  end
-
-    def test2
-    end
-
-  def test3
-
-  end
-  
-  def test4
-  end
-
-  def test5
-
-  end
-  
-  def test6
-  end
 end

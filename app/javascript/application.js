9936cdacd0cbd8f37e931a19b6995f6e0d2a860a
FlyRexy <eferovnik@mail.ru>
2023-02-07 14:19:22 +0300

    services design added(temporary)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+function click() {
+    console.log(document.getElementsByClassName('services__names')[0])
+    document.getElementsByClassName('services__names')[0].style = 'display: none'
+}

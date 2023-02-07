b384677d860b0672191ed014a84b6972208ddb86
CodeIgor31 <yagmurov.igor@mail.ru>
2023-02-07 17:15:48 +0300

    I18n and admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,73 @@
 # frozen_string_literal: true
-ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
+
+  content title: proc { I18n.t('active_admin.dashboard') } do
+    div class: 'blank_slate_container', id: 'dashboard_default_message' do
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel 'Последние изменения в отделе "Мужской зал"' do
+          ul do
+            ManHairCut.order('id DESC').limit(5).map do |rec|
+              li link_to(rec.title, admin_man_hair_cut_path(rec))
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+      column do
+        panel 'Последние изменения в отделе "Женский зал"' do
+          ul do
+            WomenHairCut.order('id DESC').limit(5).map do |rec|
+              li link_to(rec.title, admin_women_hair_cut_path(rec))
+            end
+          end
+        end
+      end
+
+      column do
+        panel 'Последние изменения в отделе "Макияж"' do
+          ul do
+            Makeup.order('id DESC').limit(5).map do |rec|
+              li link_to(rec.title, admin_makeup_path(rec))
+            end
+          end
+        end
+      end
+
+      column do
+        panel 'Последние изменения в отделе "Маникюр"' do
+          ul do
+            Manicure.order('id DESC').limit(5).map do |rec|
+              li link_to(rec.title, admin_manicure_path(rec))
+            end
+          end
+        end
+      end
+
+      column do
+        panel 'Последние изменения в отделе "Педикюр"' do
+          ul do
+            Pedicure.order('id DESC').limit(5).map do |rec|
+              li link_to(rec.title, admin_pedicure_path(rec))
+            end
+          end
+        end
+      end
+
+      column do
+        panel 'Последние изменения в отделе "Женская депиляция"' do
+          ul do
+            Depilation.order('id DESC').limit(5).map do |rec|
+              li link_to(rec.title, admin_depilation_path(rec))
+            end
+          end
+        end
+      end
+
+    end
+  end
 end
